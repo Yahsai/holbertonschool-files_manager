edeed4d5549da60588e91c81fbdaef1210b42587
Yahsai <yahsaisantana1@gmail.com>
2024-11-20 01:31:05 -0400

    Fixed task 0

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,18 +1,9 @@
-import { createClient } from 'redis';
-import { promisify } from 'util';
-import { v4 as uuidv4 } from 'uuid';
+import redis from 'redis';
 
 class RedisClient {
   constructor() {
-    // localhost:6739
-    this.client = createClient();
-    this.client.on('error', (error) => {
-      console.log(`ERROR: ${error}`);
-    });
-
-    this.client.get = promisify(this.client.get);
-    this.client.set = promisify(this.client.set);
-    this.client.del = promisify(this.client.del);
+    this.client = redis.createClient();
+    this.client.on('error', (error) => console.error(`Redis client error: ${error}`));
   }
 
   isAlive() {
@@ -20,52 +11,33 @@ class RedisClient {
   }
 
   async get(key) {
-    return this.client.get(key);
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) reject(err);
+        else resolve(reply);
+      });
+    });
   }
 
   async set(key, value, duration) {
-    await this.client.set(key, value, 'EX', duration);
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) reject(err);
+        else resolve(reply);
+      });
+    });
   }
 
   async del(key) {
-    await this.client.del(key);
-  }
-
-  /**
-   * Returns the user ID `mongodb.ObjectID` corresponding to `auth_${userSessionToken}`,
-   * from the Redis DB.
-   * If something goes wrong, if the key doesn't exist in the DB,
-   * or if the key has no value,
-   * this method returns null.
-   */
-  async getUserId(userSessionToken) {
-    return this.client.get(`auth_${userSessionToken}`);
-  }
-
-  /**
-   * Creates user session token uuidv4,
-   * and adds it to the Redis DB as `auth_${userSessionToken}`
-   * as a key with `userId` as the value.
-   *
-   * The session token will expire in the next 24h.
-   *
-   * Returns { dbResponse, userSessionToken };
-   */
-  async makeUserSession(userId) {
-    const userSessionToken = uuidv4();
-    const dbResponse = await this.client.set(`auth_${userSessionToken}`, userId, 'EX', 60 * 60 * 24);
-
-    return { dbResponse, userSessionToken };
-  }
-
-  /**
-   * Attempts to delete `auth_${userSessionToken}` from the RedisDB.
-   * Returns the DB's response.
-   */
-  async endUserSession(userSessionToken) {
-    return this.client.del(`auth_${userSessionToken}`);
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) reject(err);
+        else resolve(reply);
+      });
+    });
   }
 }
 
 const redisClient = new RedisClient();
+
 export default redisClient;
