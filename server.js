51d73dff526496ad9b407e0a2b97be8d2105a781
Yahsai <yahsaisantana1@gmail.com>
2024-11-20 01:25:35 -0400

    Task 1 fixed

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-import express from 'express';
-import routes from './routes/index';
-
-const port = process.env.PORT || 5000;
+const express = require('express');
+const routes = require('./routes');
 
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 
-for (const [request, callback] of Object.entries(routes)) {
-  const [method, route] = request.split(' ');
+app.use(routes);
 
-  if (method === 'GET') app.get(route, callback);
-  if (method === 'POST') app.post(route, callback);
-  if (method === 'PUT') app.put(route, callback);
-}
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
 
-app.listen(port, () => undefined);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
